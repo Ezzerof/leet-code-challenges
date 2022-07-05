f152cab71961b35b1e4bd348123997aecbfd8177
Ezzerof <christian.bitca@gmail.com>
2022-07-05 20:42:20 +0100

    Initial commit

---
+++ src/RomanToInt.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RomanToInt {
+
+    public static int romanToInt(String s){
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int result = map.get(s.charAt(s.length()-1));
+        for (int i = s.length()-2; i >=0 ; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String s = input.next();
+        System.out.println(romanToInt(s));
+
+    }
+}
