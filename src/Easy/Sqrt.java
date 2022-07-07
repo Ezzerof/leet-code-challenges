9ce69eb2090d1d10e99b2520228b9da45c7b611c
Ezzerof <christian.bitca@gmail.com>
2022-07-07 18:55:07 +0100

    Sqrt

---
+++ src/Easy/Sqrt.java
@@ -0,0 +1,16 @@
+package Easy;
+
+
+public class Sqrt {
+
+    public static int mySqrt(int x){
+        int result = (int)Math.sqrt(x);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(mySqrt(8));
+    }
+}
