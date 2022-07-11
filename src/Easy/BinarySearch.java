bac3a1a566d87b51d7a8b8e498f4a18cd1f9cdc0
Ezzerof <christian.bitca@gmail.com>
2022-07-11 13:34:01 +0100

    BinarySearch

---
+++ src/Easy/BinarySearch.java
@@ -0,0 +1,33 @@
+package Easy;
+
+public class BinarySearch {
+
+    static int search(int[] nums, int target) {
+        if (nums.length == 0)
+            return -1;
+
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left <= right){
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] > target) {
+                right = middle -1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = {-1,0,3,5,9,12};
+
+        search(a, 9);
+
+    }
+}
