3be93c968fd397ed7528274c31587a881beca0ee
Ezzerof <christian.bitca@gmail.com>
2022-07-12 15:09:34 +0100

    Isomorphic Strings

---
+++ src/Easy/IsomorphicStrings.java
@@ -0,0 +1,31 @@
+package Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface IsomorphicStrings {
+
+    static boolean isIsomorphic(String s, String t){
+
+        char[] sValues = new char[256];
+        char[] tValues = new char[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if (sValues[sChar] == 0 && tValues[tChar] == 0){
+                sValues[sChar] = tChar;
+                tValues[tChar] = sChar;
+            } else if (sValues[sChar] != tChar)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isIsomorphic("egg", "add"));
+    }
+}
