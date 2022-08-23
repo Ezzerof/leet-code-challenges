d57333b2d773c3bd57714b5649645f91b7c44a91
Ezzerof <christian.bitca@gmail.com>
2022-08-23 10:10:25 +0100

    1480

---
+++ src/Easy/RunningSumOf1DArray.java
@@ -0,0 +1,21 @@
+package Easy;
+
+public class RunningSumOf1DArray {
+    public static int[] runningSum(int[] nums) {
+        int temp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            temp += nums[i];
+            nums[i] = temp;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+
+        for(int i : runningSum(nums)) {
+            System.out.println(i);
+        }
+
+    }
+}
