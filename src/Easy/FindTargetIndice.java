6795d6ae2fe7050c651abce4edb338c93308c28b
Ezzerof <christian.bitca@gmail.com>
2022-08-23 09:45:15 +0100

    2089

--- src/Easy/FindTargetIndice.java
+++ src/Easy/FindTargetIndice.java
@@ -15,7 +15,7 @@ public class FindTargetIndice {
 //        int len = nums.length;
 //
 //        for (int i = 0; i < len - 1; ++i) {
-//            for (int j = 0; j < len - 1; ++j) {
+//            for (int j = 0; j < len - i - 1; ++j) {
 //                if (nums[j+1] < nums[j]) {
 //                    int temp = nums[j];
 //                    nums[j] = nums[j+1];
