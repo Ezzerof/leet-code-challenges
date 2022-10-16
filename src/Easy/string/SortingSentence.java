ff57becd409af64a59e55ee5eae38b511f1b7829
Ezzerof <christian.bitca@gmail.com>
2022-10-16 16:03:29 +0100

    1859

--- src/Easy/string/SortingSentence.java
+++ src/Easy/string/SortingSentence.java
@@ -1,55 +1,19 @@
 package Easy.string;
 
-import java.util.Arrays;
-
 public class SortingSentence {
 
     public String sortSentence(String s) {
 
         String[] sentenceArr = s.split(" ");
-        int currentDigit;
-        int nextDigit;
+        String[] newSentence = new String[sentenceArr.length];
 
-        boolean isOn = true;
-
-        while (isOn) {
-            isOn = false;
-            for (int i = 0; i < sentenceArr.length - 1; i++) {
-
-                currentDigit = findNumber(sentenceArr[i]);
-                nextDigit = findNumber(sentenceArr[i + 1]);
-
-                if (currentDigit > nextDigit) {
-                    isOn = true;
-                    String temp = sentenceArr[i];
-                    sentenceArr[i] = sentenceArr[i + 1];
-                    sentenceArr[i + 1] = temp;
-                }
-
-            }
-        }
-        String result = "";
-
-        for (String st : sentenceArr) {
-            result += st.replaceAll("[0-9]", "") + "";
+        for (String word : sentenceArr) {
+            int i = Character.getNumericValue(word.charAt(word.length()-1));
+            newSentence[i - 1] = word.substring(0, word.length() - 1);
         }
 
-        return result.trim();
+        return String.join(" ", newSentence);
     }
-
-    public int findNumber(String word) {
-        int num = 0;
-
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            if (Character.isDigit(ch)) {
-                num = Integer.parseInt(String.valueOf(ch));
-            }
-        }
-
-        return num;
-    }
-
 
     public static void main(String[] args) {
         SortingSentence st = new SortingSentence();
