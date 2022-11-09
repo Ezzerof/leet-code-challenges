79cd56d9d6dc429890ec4819b8873e7695a050d4
Ezzerof <christian.bitca@gmail.com>
2022-11-09 19:03:54 +0000

    2418

--- src/Easy/SortThePeople.java
+++ src/Easy/SortThePeople.java
@@ -1,10 +1,31 @@
 package Easy;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class SortThePeople {
 
     public String[] sortPeople(String[] names, int[] heights) {
+
+//        boolean isOn = true;
+//        while (isOn) {
+//            isOn = false;
+//            for (int i = 0; i < heights.length - 1; ++i) {
+//                if (heights[i + 1] > heights[i]) {
+//                    isOn = true;
+//                    int temp = heights[i];
+//                    heights[i] = heights[i + 1];
+//                    heights[i + 1] = temp;
+//                    String tempName = names[i];
+//                    names[i] = names[i + 1];
+//                    names[i + 1] =  tempName;
+//                }
+//            }
+//        }
+//
+//        return names;
+
+
 
         HashMap<Integer, String> list = new HashMap<>();
 
@@ -13,19 +34,21 @@ public class SortThePeople {
         }
 
         //Bubble sort
-        for (int i = 0; i < heights.length-1; i++) {
-            for (int j = 0; j < heights.length - i - 1; ++j){
+//        for (int i = 0; i < heights.length-1; i++) {
+//            for (int j = 0; j < heights.length - i - 1; ++j){
+//
+//                if (heights[j + 1] > heights[j]) {
+//                    int temp = heights[j];
+//                    heights[j] = heights[j+1];
+//                    heights[j+1] = temp;
+//                }
+//            }
+//        }
 
-                if (heights[j + 1] > heights[j]) {
-                    int temp = heights[j];
-                    heights[j] = heights[j+1];
-                    heights[j+1] = temp;
-                }
-            }
-        }
+        Arrays.sort(heights);
 
-        for (int i = 0; i < heights.length; i++) {
-            names[i] = list.get(heights[i]);
+        for (int i = names.length - 1; i >= 0; i--) {
+            names[names.length - 1 - i] = list.get(heights[i]);
         }
 
         return names;
