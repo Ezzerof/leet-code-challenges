8d5912192757a327ac7cac8e84763ea9dd102e71
Ezzerof <christian.bitca@gmail.com>
2023-05-11 16:06:17 +0100

    reverse between

---
+++ src/Medium/list_node/ReverseBetween.java
@@ -0,0 +1,29 @@
+package Medium.list_node;
+
+import java.util.List;
+
+public class ReverseBetween {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+
+        ListNode leftNode = prev.next;
+
+        for (int i = 0; i < right - left; i++) {
+            ListNode toChange = leftNode.next;
+            leftNode.next = toChange.next;
+            toChange.next = prev.next;
+            prev.next = toChange;
+        }
+
+        return dummy.next;
+    }
+
+}
